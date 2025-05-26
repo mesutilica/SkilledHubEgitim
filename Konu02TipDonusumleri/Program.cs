6424c924d942574c5540b6bcc9bebd5cbd8b1ae8
Mesut ILICA <mesut.ilica@gmail.com>
2025-05-26 20:26:48 +0300

    Add project files.

---
+++ Konu02TipDonusumleri/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Konu02TipDonusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tip Dönüşümleri!");
+            //Implicit Casting - otomatik dönüşüm
+            //char -> int -> long -> float -> double
+            //Explicit Casting - Manuel
+            //double -> float -> long -> int -> char
+
+            //Implicit Casting daha küçük boyutlu bir türü daha büyük bir boyut türüne geçirirken otomatik olarak yapılır
+            Console.WriteLine("Implicit Casting");
+            int sayi = 9;
+            double kesirliSayi = sayi; //int den double a otomatik dönüşüm
+
+            Console.WriteLine(sayi);      // Çıktı 9
+            Console.WriteLine(kesirliSayi);   // Çıktı 9
+
+
+            //Explicit Casting türü değerin önüne parantez içine alarak manuel yapılmalıdır
+            Console.WriteLine("Explicit Casting");
+            double kesirliSayi2 = 9.78;
+            int tamSayi = (int)kesirliSayi2;    // Manuel dönüştürme: double dan int e
+            Console.WriteLine("kesirliSayi2:" + kesirliSayi2);
+            Console.WriteLine("tamSayi:" + tamSayi);
+            Console.WriteLine();
+        }
+    }
+}
