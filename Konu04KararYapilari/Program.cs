0220328bcf762f2f5e220a340bc331c2dd04f615
Mesut ILICA <mesut.ilica@gmail.com>
2025-05-27 22:01:46 +0300

    27-05-2025 karar yapıları

---
+++ Konu04KararYapilari/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Konu04KararYapilari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Karar Yapilari!");
+            Console.WriteLine("Bir Sayı Giriniz:");
+            var sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0)//eğer sayı 0 dan büyükse
+            {
+                Console.WriteLine("Sayı Pozitif");
+            }
+            else if (sayi < 0) // değilse eğer sayı 0 dan küçükse
+            {
+                Console.WriteLine("Sayı Negatif");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Sıfır");
+            }
+        }
+    }
+}
