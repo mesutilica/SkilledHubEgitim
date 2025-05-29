0c56b4394edb78024e00db1630f44d4d2bd97553
Mesut ILICA <mesut.ilica@gmail.com>
2025-05-29 22:01:28 +0300

    29-05-2025 diziler

--- Konu05Metotlar/Program.cs
+++ Konu05Metotlar/Program.cs
@@ -6,12 +6,28 @@
         {
             Console.WriteLine("Metotlar!");
             Console.WriteLine();
+            Console.Beep(); // bip sesi çıkar
+            //Console.BackgroundColor = ConsoleColor.Green;
             ToplamaYap(); // Metot çağrısı olmadan ekrana yazdırmaz
             ToplamaYap(50, 8);
             Console.WriteLine("Geriye değer döndüren metot:");
             int sonuc = ToplamaYap(10, 20, 30); // Metot çağrısı ile geriye değer döndürülür
             Console.WriteLine("Sonuç: " + sonuc);
+            /*
+            Console.WriteLine("Lütfen mail adresinizi giriniz");
+            var email = Console.ReadLine();
+            if (MailGonder(email) == false)
+            {
+                Console.WriteLine("Mail Adresi Boş Geçilemez!");
+            }
+            else 
+                Console.WriteLine("Mail kabul edildi!");
+            */
+            // KdvHesapla();
+            MatematikselMetotlar();
         }
+
+        #region ToplamaYapMetotları
         static void ToplamaYap()
         {
             Console.WriteLine(10 + 8);
@@ -29,6 +45,39 @@
             //metodun aldığı parametre ile geri dönüş veri türü aynı olmak zorunda DEĞİLDİR!
             //Yani bir metot parametre olarak int alırken geriye string, bool vb veri türü döndürebilir!
             return sayi1 + sayi2 + sayi3; // int olan metotlar geriye değer döndürür, return ile değer döndürülür
+        }
+        #endregion
+
+        static bool MailGonder(string mailAdresi)
+        {
+            if (!string.IsNullOrWhiteSpace(mailAdresi))//Eğer mailAdresi değişkenine bir değer girilmişse, yani mail adresi boş değilse
+            {
+                return true;//geriye true değeri döndür
+            }
+            else 
+                return false;//mailAdresi değişkeni boşsa geriye false döndür
+        }
+        static void KdvHesapla()
+        {
+            double fiyat, kdv, toplam;
+            Console.WriteLine("Ürün fiyatını giriniz:");
+            fiyat = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kdv miktarını giriniz:");
+            var kdvMiktari = Convert.ToDouble(Console.ReadLine());
+            kdv = fiyat * kdvMiktari / 100;
+            toplam = fiyat + kdv;
+            Console.WriteLine("Ürün kdv tutarı: " + kdv + " TL");
+            Console.WriteLine("Kdv dahil fiyatı : " + toplam + " TL");
+        }
+        static void MatematikselMetotlar()
+        {
+            // belirli matematiksel işlemleri yapan bazı metotlar
+            int max = Math.Max(10, 34); // x ve y sayılarının en büyüğünü tutar.
+            int min = Math.Min(10, 34); // x ve y sayılarının en küçüğünü tutar.
+
+            double c = Math.Ceiling(12.67); //x sayısından büyük en küçük tam sayıyı tutar (yukarı yuvarlama).
+            double d = Math.Floor(12.67); //x sayısından küçük en büyük tam sayıyı tutar (aşağı yuvarlama).
+            Console.WriteLine("Max:" + max + " min:" + min + " Ceiling:" + c + " Floor:" + d);
         }
     }
 }
---
+++ Konu06Diziler/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Konu06Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diziler!");
+            //Dizi oluşturma
+            int[] sayi;
+            int[] ogrenciler = new int[6];//ogrenciler isminde int veri tipi taşıyan ve 7 elemandan oluşan bir dizi oluşturduk. Dizi boyutu artmaz, azalmaz.
+            //Dizilerde indis denilen yapı vardır ve içine eklenecek elemanlar 0 dan başlar
+            ogrenciler[0] = 100; // diziye veri atama
+            ogrenciler[1] = 200;
+            ogrenciler[2] = 300;
+            ogrenciler[3] = 400;
+            ogrenciler[4] = 500;
+            ogrenciler[5] = 600;
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler[5]);
+        }
+    }
+}
