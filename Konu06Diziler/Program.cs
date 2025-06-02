76e8654943603238d51ffd591feef0ad53c92b5e
Mesut ILICA <mesut.ilica@gmail.com>
2025-06-02 22:02:57 +0300

    2-6-25 döngüler

--- Konu06Diziler/Program.cs
+++ Konu06Diziler/Program.cs
@@ -16,6 +16,39 @@
             ogrenciler[4] = 500;
             ogrenciler[5] = 600;
             Console.WriteLine("Seçilen öğrenci no: " + ogrenciler[5]);
+            ogrenciler[5] = 550;
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler[5]);
+            // ogrenciler[6] = 666; //Dizilere başlangıçta kaç elemandan oluşacağı tanımlanmışsa o sayının dışına çıktığımızda hata alırız!
+
+            Console.WriteLine();
+
+            string[] isimler = new string[5];
+            isimler[0] = "Alp";
+            isimler[1] = "Ali";
+            isimler[2] = "Enes";
+            isimler[3] = "Evrim";
+            isimler[4] = "Sümeyye";
+
+            Console.WriteLine("isimler[4]: " + isimler[4]);
+
+            Console.WriteLine();
+
+            int[] ogrenciler2 = { 100, 200, 300, 400, 500 };
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler2[3]);
+            ogrenciler2[3] = 550;
+            Console.WriteLine("Seçilen öğrenci no: " + ogrenciler2[3]);
+
+            Console.WriteLine();
+
+            string[] kategoriler = { "Bilgisayar", "Elektronik", "Cep Telefonu", "Beyaz Eşya", "Kitap" };//Diziye eklenecek kayıt sayısı belirsizse bu şekilde de dizi tanımlaması yapabiliriz
+            Console.WriteLine("kategoriler 1 : " + kategoriler[1]);
+            kategoriler[1] = "Aksesuar";
+            Console.WriteLine("kategoriler 1 : " + kategoriler[1]);
+
+            Console.WriteLine();
+
+            string[] urunler = { "Ürün 1", "Ürün 2", "Ürün3" };
+            Console.WriteLine(urunler[1]);
         }
     }
 }
---
+++ Konu07Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Konu07Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dönguler!");
+
+            Console.WriteLine("1-For Döngüsü");
+            for (int i = 0; i < 5; i++) //i değişkeni oluştur ve 0 değerini ata; i 5 den küçük olduğu sürece dön; her dönüşte i yi 1 artır i++ ile
+            {
+                Console.WriteLine("i değişkeninin değeri {0}", i);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("2-While Döngüsü");
+            int j = 0; // dışarıda bir değişken tanımlıyoruz
+            while (j < 5) //While döngüsünün şartı bu şekilde, anlamı toplam küçükse 5 den sürekli dön
+            {
+                Console.WriteLine("While Sayı {0} değeri", j);//toplamın değerini ekrana yazdırıyoruz
+                j++;//toplam sayısını arttırıyoruz ki sonsuz döngüye girmesin program
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("3-Do While Döngüsü");
+            int toplam = 7;
+            do
+            {
+                Console.WriteLine("toplamın değeri : " + toplam);
+            } while (toplam < 5);
+        }
+    }
+}
