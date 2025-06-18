5fe3b3af37d0aa58b7afedda455d57b6d8f660a9
Mesut ILICA <mesut.ilica@gmail.com>
2025-06-18 22:01:14 +0300

    18-06-2025 Polimorfizm - Çokbiçimlilik

---
+++ Konu11Enumlar/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Konu11Enumlar
+{
+    internal class Program
+    {
+        //Enum(Numaratör) lar nesneleri numaralandırmak için kullanılır
+        /*
+         * Enum tipler üzerindeki kısıtlar
+         * 1-Enum blokunda metot tanımlanamaz
+         * 2-Arayüz(Interface) kullanamazlar
+         * 3-enum blokunda property kullanılmaz         
+         */
+        enum Aylar : byte//byte koleksiyondaki numaraların veri tipinin byte türünden olacağını belirtiyor
+        {
+            Ocak, Şubat, Mart, Nisan, Mayıs, Haziran, Temmuz, Ağustos, Eylül, Ekim, Kasım, Aralık
+        }
+        enum SiparisDurumu
+        {
+            Hazırlanıyor, Hazırlandı, KargoBekleniyor, Kargolandı
+        }
+        enum Meyveler : int
+        {
+            Elma = 3, Armut = 7, Çilek = 1
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu 11 Enumlar!");
+            byte a = (byte)Meyveler.Armut;
+            byte b = (byte)Meyveler.Elma;
+            byte c = (byte)Meyveler.Çilek;
+            Console.WriteLine($"{Meyveler.Armut} = {a}, {Meyveler.Elma}={b}, {Meyveler.Çilek}={c}");
+            Ornek1(SiparisDurum: 1);
+
+        }
+        static void Ornek1(int SiparisDurum)
+        {
+            if (SiparisDurum == (int)SiparisDurumu.Hazırlanıyor)
+                Console.WriteLine("Siparişiniz Hazırlanıyor");
+            if (SiparisDurum == (int)SiparisDurumu.Hazırlandı)
+                Console.WriteLine("Siparişiniz Hazırlandı");
+            if (SiparisDurum == (int)SiparisDurumu.KargoBekleniyor)
+                Console.WriteLine("Siparişiniz İçin Kargo Bekleniyor");
+            if (SiparisDurum == (int)SiparisDurumu.Kargolandı)
+                Console.WriteLine("Siparişiniz Kargoya Verildi");
+        }
+    }
+}
---
+++ Konu12KalitimInheritance/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Konu12KalitimInheritance
+{
+    class Arac
+    {
+        public string AracTuru;
+        public void KornaCal()
+        {
+            Console.WriteLine("Kornaya Basıldı!");
+        }
+    }
+    class Otomobil : Arac // iki nokta üstü üste araç dediğimizde araç sınıfındaki içerikler otomobil sınıfında kullanılabilir.
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+    }
+    class Yat : Arac
+    {
+        public int Uzunluk { get; set; }
+        public int Kamara { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu12 Kalitim Inheritance!");
+            Arac arac = new();
+            arac.AracTuru = "Araba";
+            Console.WriteLine("arac.AracTuru = " + arac.AracTuru);
+
+            Otomobil otomobil = new();
+            otomobil.AracTuru = "Otomobil"; // Normalde Otomobil classında AracTuru yok
+            otomobil.Marka = "Togg";
+            otomobil.Model = "T10X";
+            Console.WriteLine("otombil.AracTuru : " + otomobil.AracTuru);
+            otomobil.KornaCal(); // KornaCal metodu bir üst sınıf olan Arac sınıfından geliyor
+
+            Console.WriteLine();
+
+            Kategori kategori = new()
+            {
+                Name = "Elektronik",
+                UstMenudeGoster = true,
+                CreateDate = DateTime.Now
+            };
+            if (kategori.UstMenudeGoster == true)
+            {
+                Console.WriteLine($"kategori bilgileri :\n Adı : {kategori.Name} - Eklenme Tarihi : {kategori.CreateDate}");
+            }
+
+            Console.WriteLine();
+
+            Urun urun = new()
+            {
+                Name = "Klavye",
+                Fiyat = 299,
+                Kdv = 18,
+                CreateDate = DateTime.Now
+            };
+            Console.WriteLine("Ürün Bilgileri");
+            Console.WriteLine($"Adı: {urun.Name}");
+            Console.WriteLine($"Fiyat: {urun.Fiyat}");
+            Console.WriteLine($"Kdv: {urun.Kdv}");
+            Console.WriteLine($"Eklenme Tarihi: {urun.CreateDate}");
+        }
+        // Polimorfizm - Çokbiçimlilik
+        public class Cizici
+        {
+            public virtual void Ciz() // virtual keywordü ile bu metodu override-ezilebilir hale getiriyoruz
+            {
+                Console.WriteLine("Cizici");
+            }
+        }
+        public class DogruCiz : Cizici
+        {
+            public override void Ciz()
+            {
+                Console.WriteLine("Düz Çizgi");
+            }
+        }
+    }
+}
