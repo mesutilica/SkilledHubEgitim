10216ee2b4c8249be2733c514d337d97cb7d41a2
Mesut ILICA <mesut.ilica@gmail.com>
2025-06-23 22:02:23 +0300

    Konu14InterfacesArayuzler bitti

---
+++ Konu14InterfacesArayuzler/IVeritabaniIslemleri.cs
@@ -0,0 +1,13 @@
+﻿namespace Konu14InterfacesArayuzler
+{
+    internal interface IVeritabaniIslemleri
+    {
+        // bu arayüzü kullanacak class lar aşağıdaki metotları içermek zorundadır!
+        // CRUD metot imzaları
+        void Add(string name); // ekle
+        void Update(int id); // güncelle
+        void Delete(int id); // sil
+        void GetAll(); // listele
+        // interface lerde metotlar açıkça yazılmaz, sadece geri dönüş tipi, adı ve parametresi belirtilir.
+    }
+}
