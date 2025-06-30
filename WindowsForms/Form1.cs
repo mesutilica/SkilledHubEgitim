1a8deff0b3098c71b161a080be2efd9dd019a266
Mesut ILICA <mesut.ilica@gmail.com>
2025-06-30 22:00:41 +0300

    30-06-2025 win forms

---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ekrandaki form yüklendiğinde bu metot çalışır, veritabanından veri çekip ekrana gönderme gibi işlemleri burada yapabiliriz.
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            // form2.Show(); //Çağırılan form Show metoduyla gösterilirse, tüm formlara erişim sağlanabilir
+            form2.ShowDialog(); //Çağırılan form ShowDialog metoduyla gösterilirse, üstteki form kapatılana kadar alttaki forma erişim engellenir
+            // this.Hide(); // form 2 yi açtıktan sonra bu formu (form 1) gizle
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // this.Close(); // formu kapat
+            Application.Exit(); // uygulmayı kapat
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.AdSoyad = txtAdSoyad.Text;
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ WindowsForms/Form3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;//timer1 nesnesini sayfa yüklenirken aktif hale getiriyoruz
+            timer1.Interval = 1000;//timer ın süresini milisaniye cinsinden bu şekile ayarlayabiliriz
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random renk = new Random();//renk isminde rasgele sayı üreten nesne oluşturuldu
+            this.BackColor = Color.FromArgb(renk.Next(1, 100), renk.Next(1, 100), renk.Next(1, 100));//Burada this sınıfı geçerli formu i temsil ediyor. Formun arkaplan rengine rasgele 1 ile 100 arası RGB renk kodlarını oluşturacak sayılar üretilecek
+        }
+    }
+}
