f2c4a9ae91329af126b4ac15c90e9c4a7ee11c65
Mesut ILICA <mesut.ilica@gmail.com>
2025-07-14 22:03:03 +0300

    Ado.Net list

---
+++ WindowsFormsAppAdoNet/OrtakDAL.cs
@@ -0,0 +1,19 @@
+﻿using System.Data; // veritabanı işlemleri için gerekli
+using System.Data.SqlClient; // ado.net kütüphaneleri
+
+namespace WindowsFormsAppAdoNet
+{
+    public class OrtakDAL
+    {
+        internal SqlConnection _connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB; database=UrunYonetimiAdoNet; Integrated Security=True;"); // veritabanımın bulunduğu server bilgisini connection string olarak burada tanımlıyorum.
+
+        internal void ConnectionKontrol()//Veritabanı bağlantımızı kontrol edecek olan metot
+        {
+            if (_connection.State == ConnectionState.Closed)//Eğer veritabanı bağlantımızın durumu kapalı ise
+            {
+                _connection.Open();//Veritabanı bağlantısını aç
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsAppAdoNet/ProductDal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data; // veritabanı işlemleri için gerekli
+using System.Data.SqlClient; // ado.net kütüphaneleri
+
+namespace WindowsFormsAppAdoNet
+{
+    public class ProductDal : OrtakDAL
+    {
+        public List<Product> GetAll()
+        {
+            var products = new List<Product>();
+
+            ConnectionKontrol();
+
+            SqlCommand command = new SqlCommand("select * from Urunler", _connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read()) // reader nesnesi içerisinde okunacak kayıt olduğu sürece
+            {
+                var product = new Product()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    UrunAdi = reader["UrunAdi"].ToString(),
+                    StokMiktari = Convert.ToInt32(reader["StokMiktari"]),
+                    UrunFiyati = Convert.ToDecimal(reader["UrunFiyati"]),
+                    Durum = Convert.ToBoolean(reader["Durum"])
+                };
+                products.Add(product); // db den okunan ürünü listeye ekle
+            }
+            reader.Close(); // veritabanından okuyucuyu kapat
+            _connection.Close(); // veritabanı bağlantısını kapat
+            command.Dispose(); // sql komut nesnesini yoket
+
+            return products;
+        }
+    }
+}
