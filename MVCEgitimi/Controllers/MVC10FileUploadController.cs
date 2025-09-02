0ad472b3367e714eb545de68ee695d47a4b72a65
Mesut ILICA <mesut.ilica@gmail.com>
2025-09-02 22:37:04 +0300

    Kuki-çerezler

--- MVCEgitimi/Controllers/MVC10FileUploadController.cs
+++ MVCEgitimi/Controllers/MVC10FileUploadController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace MVCEgitimi.Controllers
 {
@@ -25,17 +26,41 @@ namespace MVCEgitimi.Controllers
                 if (uzanti == ".jpg" || uzanti == ".jpeg" || uzanti == ".png" || uzanti == ".gif") // Sadece bu uzantılardaki dosyaları kabul et
                 {
                     // 1. Yöntem Random(Rastgele) İsimle Dosya Yükleme
+                    /*
                     var randomFileName = Path.GetRandomFileName(); // rasgele dosya ismi oluşturma metodu
                     var fileName = Path.ChangeExtension(randomFileName, ".jpg"); // dosya adı ve uzantısını değiştirip birleştirdik
                     var path = Path.Combine(klasor, fileName); // klasör ve resim adını birleştirdik
                     using var stream = new FileStream(path, FileMode.Create);
                     dosya.CopyTo(stream); // resmi sunucuya yükledik
                     TempData["Resim"] = fileName;
+                    */
+
+                    // 2. Yöntem - Resmi Kendi Adıyla Yükleme
+                    /*
+                    var dosyaAdi = Path.GetFileName(dosya.FileName);
+                    var yol = Path.Combine(klasor, dosyaAdi);
+                    using var stream = new FileStream(yol, FileMode.Create);
+                    dosya.CopyTo(stream); // resmi sunucuya yükledik
+                    TempData["Resim"] = dosyaAdi;
+                    */
+                    using var stream = new FileStream(klasor + dosya.FileName, FileMode.Create); // Buradaki using ifadesi stream isimli değişkenin işinin bittikten sonra bellekten atılmasını sağlar
+                    dosya.CopyTo(stream); // resmi sunucuya yükledik
+                    TempData["Resim"] = dosya.FileName;
                 }
                 else
                     TempData["Message"] = " Sadece Resim Dosyası Yükleyebilirsiniz! ";
             }
             return View();
+        }
+        [HttpPost]
+        public IActionResult ResimSil(string resimYolu)
+        {
+            if (System.IO.File.Exists(resimYolu)) // eğer gelen adreste böyle bir dosya varsa
+            {
+                System.IO.File.Delete(resimYolu);
+                TempData["message"] = "Resim Silindi!";
+            }
+            return RedirectToAction("Index");
         }
     }
 }
